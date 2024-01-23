015674805ef8d54354b761c071fac27eaae0d2b1
TessyJames28 <tessyjames28@gmail.com>
2024-01-23 21:55:41 +0100

    made adjustments per the changes in the modules

--- Inheritance/program.py
+++ Inheritance/program.py
@@ -1,10 +1,15 @@
 import hr_system
+import employees
 
-salary_employee = hr_system.SalaryEmployee(1, "John Smith", 1500)
-hourly_employee = hr_system.HourlyEmployee(2, "Jane Doe", 40, 15)
-commission_employee = hr_system.CommissionEmployee(3, "Kevin Bacon", 1000, 250)
+salary_employee = employees.SalaryEmployee(1, "John Smith", 1500)
+hourly_employee = employees.HourlyEmployee(2, "Jane Doe", 40, 15)
+commission_employee = employees.CommissionEmployee(3, "Kevin Bacon", 1000, 250)
 
 payroll_system = hr_system.PayrollSystem()
 payroll_system.calculate_payroll(
-    [salary_employee, hourly_employee, commission_employee]
+    [
+        salary_employee,
+        hourly_employee, 
+        commission_employee,
+    ]
 )
