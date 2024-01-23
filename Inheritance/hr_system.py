61291c0b05e2c1c10ba91602b93ce54b04a0664d
TessyJames28 <tessyjames28@gmail.com>
2024-01-23 14:27:25 +0100

    The HR System

---
+++ Inheritance/hr_system.py
@@ -0,0 +1,53 @@
+# Payroll system class that process employees payrool
+class PayrollSystem:
+    def calculate_payroll(self, employees):
+        print("Calculating Payroll")
+        print("===================")
+        for employee in employees:
+            print(f"Payroll for {employee.id} - {employee.name}")
+            print(f"- Check amount: {employee.calculate_payroll()}")
+            print("")
+            
+
+# a bass class Employee that handles the common interface for every employee type
+class Employee:
+    def __init__(self, id, name):
+        self.id = id
+        self. name = name
+        
+        
+# the system requires that every employee procesed must provide a .calculate_payroll() interface
+# that returns the weekly salary for the employee.
+
+class SalaryEmployee(Employee):
+    def __init__(self, id, name, weekly_salary):
+        super().__init__(id, name)
+        self.weekly_salary = weekly_salary
+        
+    def calculate_payroll(self):
+        return self.weekly_salary
+    
+    
+# for employees paid hourly
+
+class HourlyEmployee(Employee):
+    def __init__(self, id, name, hours_worked, hourly_rate):
+        super().__init__(id, name)
+        self.hours_worked = hours_worked
+        self.hourly_rate = hourly_rate
+        
+    def calculate_payroll(self):
+        return self.hours_worked * self.hourly_rate
+    
+# for sales associate who are paid fixed salary plus commission based on their sales
+
+class CommissionEmployee(SalaryEmployee):
+    def __init__(self, id, name, weekly_salary, commission):
+        super().__init__(id, name, weekly_salary)
+        self.commission = commission
+        
+    def calaculate_payroll(self):
+        fixed = super().calaculate_payroll()
+        return fixed + self.commission
+    
+    
