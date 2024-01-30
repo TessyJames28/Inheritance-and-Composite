65e94d1d5910ecea15272b98c843d74af8f17b07
TessyJames28 <tessyjames28@gmail.com>
2024-01-30 13:22:28 +0100

    Updated the program file to handle productivity interface

--- Inheritance/program.py
+++ Inheritance/program.py
@@ -1,15 +1,21 @@
 import hr_system
 import employees
+import productivity
 
-salary_employee = employees.SalaryEmployee(1, "John Smith", 1500)
-hourly_employee = employees.HourlyEmployee(2, "Jane Doe", 40, 15)
-commission_employee = employees.CommissionEmployee(3, "Kevin Bacon", 1000, 250)
+manager = employees.Manager(1, "John MacLean", 3000)
+secretary = employees.Secretary(2, "John Smith", 1500)
+sales_lady = employees.SalesPerson(3, "Jane Doe", 1000, 250)
+factory_worker = employees.FactoryWorker(4, "Kevin Bacon", 40, 15)
+
+employees = [
+    manager,
+    secretary,
+    sales_lady,
+    factory_worker
+]
+
+productivity_system = productivity.ProductivitySystem()
+productivity_system.track(employees, 40)
 
 payroll_system = hr_system.PayrollSystem()
-payroll_system.calculate_payroll(
-    [
-        salary_employee,
-        hourly_employee, 
-        commission_employee,
-    ]
-)
+payroll_system.calculate_payroll(employees)
