9baf74c89aa3366242e0057f68299343e87fc6b1
TessyJames28 <tessyjames28@gmail.com>
2024-02-07 13:09:45 +0100

    HR system with salary policy

--- Composition/hr_system.py
+++ Composition/hr_system.py
@@ -1,4 +1,21 @@
 class PayrollSystem:
+    def __init__(self):
+        self._employee_policies = {
+            1: SalaryPolicy(3000),
+            2: SalaryPolicy(1500),
+            3: CommissionPolicy(1000, 100),
+            4: HourlyPolicy(15),
+            5: HourlyPolicy(9),
+        }
+        
+        
+    def get_policy(self, employee_id):
+        policy = self._employee_policies.get(employee_id)
+        if not policy:
+            return ValueError(employee_id)
+        return policy
+    
+    
     def calculate_payroll(self, employees):
         print("Calculating Payroll")
         print("===================")
@@ -10,26 +27,38 @@ class PayrollSystem:
                 print(employee.address)
             print("")
             
+
+class PayrollPolicy:
+    def __init__(self):
+        self.hours_worked = 0
+        
+    def track_work(self, hours):
+        self.hours_worked += hours 
+        
             
-class SalaryPolicy:
+class SalaryPolicy(PayrollPolicy):
     def __init__(self, weekly_salary):
         self.weekly_salary = weekly_salary
 
     def calculate_payroll(self):
         return self.weekly_salary
 
-class HourlyPolicy:
-    def __init__(self, hours_worked, hourly_rate):
-        self.hours_worked = hours_worked
+class HourlyPolicy(PayrollPolicy):
+    def __init__(self, hourly_rate):
         self.hourly_rate = hourly_rate
 
     def calculate_payroll(self):
         return self.hours_worked * self.hourly_rate
 
 class CommissionPolicy(SalaryPolicy):
-    def __init__(self, weekly_salary, commission):
+    def __init__(self, weekly_salary, commission_per_Sale):
         super().__init__(weekly_salary)
-        self.commission = commission
+        self.commission_per_sale = commission_per_Sale
+        
+    @property
+    def commission(self):
+        sales = self.hours_worked / 5
+        return sales * self.commission_per_sale
 
     def calculate_payroll(self):
         fixed = super().calculate_payroll()
