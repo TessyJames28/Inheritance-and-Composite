202b294444da3c300b1b592885c8f26de9c7d109
TessyJames28 <tessyjames28@gmail.com>
2024-02-10 17:26:10 +0100

    Composition in Python

--- Composition/employees.py
+++ Composition/employees.py
@@ -1,51 +1,39 @@
-from hr_system import PayrollSystem
-from productivity import ProductivitySystem
-from contacts import AddressBook
+from hr_system import SalaryPolicy, HourlyPolicy, CommissionPolicy
+from productivity import ManagerRole, SalesRole, SecretaryRole, FactoryRole
 
-
-class EmployeeDatabase:
-    def __init__(self):
-        self._employees = [
-            {"id": 1, "name": "Mary Poppins", "role": "manager"},
-            {"id": 2, "name": "John Smith", "role": "secretary"},
-            {"id": 3, "name": "Kevin Bacon", "role": "sales"},
-            {"id": 4, "name": "Jane Doe", "role": "factory"},
-            {"id": 5, "name": "Robin Williams", "role": "secretary"},
-        ]
-        self.productivity = ProductivitySystem()
-        self.payroll = PayrollSystem()
-        self.employee_addresses = AddressBook()
-
-    
-    @property
-    def employees(self):
-        return [self._create_employee(**data) for data in self._employees]
-    
-    
-    def _create_employee(self, id, name, role):
-        address = self.employee_addresses.get_employee_address(id)
-        employee_role = self.productivity.get_role(role)
-        payroll_policy = self.payroll.get_policy(id)
-        return Employee(id, name, address, employee_role, payroll_policy)
-        
 
 class Employee:
-    def __init__(self, id, name, address, role, payroll):
+    def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.address = address
-        self.role = role
-        self.payroll = payroll
+        self.address = None
         
         
-    def work(self, hours):
-        duties = self.role.perform_duties(hours)
-        print(f"Employee {self.id} - {self.name}:")
-        print(f"- {duties}")
-        print("")
-        self.payroll.track_work(hours)
+class Manager(Employee, ManagerRole, SalaryPolicy):
+    def __init__(self, id, name, weekly_salary):
+        SalaryPolicy.__init__(self, weekly_salary)
+        super().__init__(id, name)
         
         
-    def calculate_payroll(self):
-        return self.payroll.calculate_payroll()
+class Secretary(Employee, SecretaryRole, SalaryPolicy):
+    def __init__(self, id, name, weekly_salary):
+        SalaryPolicy.__init__(self, weekly_salary)
+        super().__init__(id, name)
         
+        
+class SalesPerson(Employee, SalesRole, CommissionPolicy):
+    def __init__(self, id, name, weekly_salary, commission):
+        CommissionPolicy.__init__(self, weekly_salary, commission)
+        super().__init__(id, name)
+        
+        
+class FactoryWorker(Employee, FactoryRole, HourlyPolicy):
+    def __init__(self, id, name, hours_worked, hourly_rate):
+        HourlyPolicy.__init__(self, hours_worked, hourly_rate)
+        super().__init__(id, name)
+        
+        
+class TemporarySecretary(Employee, SecretaryRole, HourlyPolicy):
+    def __init__(self, id, name, hours_worked, hourly_rate):
+        HourlyPolicy.__init__(self, hours_worked, hourly_rate)
+        super().__init__(id, name)
