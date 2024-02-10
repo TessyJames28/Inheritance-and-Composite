f8803ef8d508badb973b79bd1614db55b81cb336
TessyJames28 <tessyjames28@gmail.com>
2024-02-10 17:26:37 +0100

    The productivity interface

--- Composition/productivity.py
+++ Composition/productivity.py
@@ -1,45 +1,30 @@
 class ProductivitySystem:
-    def __init__(self):
-        self._roles = {
-            "manager": ManagerRole,
-            "secretary": SecretaryRole,
-            "sales": SalesRole,
-            "factory": FactoryRole,
-        }
-        
-        
-    def get_role(self, role_id):
-        role_type = self._roles.get(role_id)
-        if not role_type:
-            raise ValueError(role_id)
-        return role_type()
-    
-    
-    def track_work(self, employees, hours):
+    def track(self, employees, hours):
         print(f"Tracking Employee Productivity")
         print("===============================")
         for employee in employees:
-            employee.work(hours)
+            result = employee.work(hours)
+            print(f"{employee.name}: {result}")
             print("")
         print("")
         
         
 
 class ManagerRole:
-    def perform_duties(self, hours):
-        return f"screams and yells for {hours} hours."
+    def work(self, hours):
+        return f"screams and yells for {hours} hourrs."
     
 
 class SecretaryRole:
-    def perform_duties(self, hours):
-        return f"does paperwork for {hours} hours."
+    def work(self, hours):
+        return f"expends {hours} hours doing office paperwork."
     
     
 class SalesRole:
-    def perform_duties(Self, hours):
+    def work(Self, hours):
         return f"expends {hours} hours on the phone."
     
     
 class FactoryRole:
-    def perform_duties(self, hours):
+    def work(self, hours):
         return f"manufactures gadgets for {hours} hours."
