e091d5c60554253451f6e18690b71f31b024aede
TessyJames28 <tessyjames28@gmail.com>
2024-02-10 17:30:11 +0100

    Improved the producitivity system for policy-based composition

---
+++ Composition/Policy_Based_Composition/productivity.py
@@ -0,0 +1,45 @@
+class ProductivitySystem:
+    def __init__(self):
+        self._roles = {
+            "manager": ManagerRole,
+            "secretary": SecretaryRole,
+            "sales": SalesRole,
+            "factory": FactoryRole,
+        }
+        
+        
+    def get_role(self, role_id):
+        role_type = self._roles.get(role_id)
+        if not role_type:
+            raise ValueError(role_id)
+        return role_type()
+    
+    
+    def track_work(self, employees, hours):
+        print(f"Tracking Employee Productivity")
+        print("===============================")
+        for employee in employees:
+            employee.work(hours)
+            print("")
+        print("")
+        
+        
+
+class ManagerRole:
+    def perform_duties(self, hours):
+        return f"screams and yells for {hours} hours."
+    
+
+class SecretaryRole:
+    def perform_duties(self, hours):
+        return f"does paperwork for {hours} hours."
+    
+    
+class SalesRole:
+    def perform_duties(Self, hours):
+        return f"expends {hours} hours on the phone."
+    
+    
+class FactoryRole:
+    def perform_duties(self, hours):
+        return f"manufactures gadgets for {hours} hours."
