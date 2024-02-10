79e5388e576ccbc013d1a9090989041ca76d7c06
TessyJames28 <tessyjames28@gmail.com>
2024-02-10 17:27:11 +0100

    Composition-based program implementation

--- Composition/program.py
+++ Composition/program.py
@@ -1,12 +1,30 @@
-from hr_system import PayrollSystem
-from employees import EmployeeDatabase
-from productivity import ProductivitySystem
+import hr_system
+import productivity
+import employees
+import contacts
 
+manager = employees.Manager(1, "Robert MacCoy", 3000)
+manager.address = contacts.Address(
+    "121 Admin Rd", "Concord", "NH", "03301"
+)
+secretary = employees.Secretary(2, "Janet Jack", 1800)
+secretary.address = contacts.Address(
+    "67 Paperwork Ave.", "Manchester", "NH", "03101"
+)
+sales_lady = employees.SalesPerson(3, "Ginny Philip", 1500, 350)
+factory_worker = employees.FactoryWorker(4, "Nathan Gate", 40, 20)
+temporary_secretary = employees.TemporarySecretary(5, "Anne Smith", 40, 15)
 
-productivity_system = ProductivitySystem()
-payroll_system = PayrollSystem()
-employee_database = EmployeeDatabase()
+employees = [
+    manager,
+    secretary,
+    sales_lady,
+    factory_worker,
+    temporary_secretary
+]
 
-employees = employee_database.employees
-productivity_system.track_work(employees, 40)
-payroll_system.calculate_payroll(employees)
+productivity = productivity.ProductivitySystem()
+productivity.track(employees, 40)
+
+hr_system = hr_system.PayrollSystem()
+hr_system.calculate_payroll(employees)
